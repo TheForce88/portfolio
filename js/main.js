5996163cd7e64e08d5222f6ef0d4010d481bc7df
eches <Elaine.Chesoni83@gmail.com>
2018-04-18 21:22:35 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+
+  // $('.preloader-background').delay(1700).fadeOut('slow');
+  //
+	// $('.preloader-wrapper')
+	// 	.delay(1700)
+	// 	.fadeOut();
+
+  $('.sidenav').sidenav();
+  $('.carousel').carousel();
+
+  $('video').addClass('fadeIn animated');
+
+  // $('.revelio-home').on('click', function(){
+  //   $('.projects-container').hide();
+  //   $('.home-container').show();
+  // })
+  //
+  // $('.revelio-projects').on('click', function(){
+  //   // $('.sidenav').fadeOut();
+  //   $('.home-container').hide();
+  //   $('.projects-container').show();
+  // })
+
+});
